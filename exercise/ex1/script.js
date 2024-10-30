9d3ed0db4f107dd883927942cfb6f661cf256d52
Myrzabek Kaisar <myrzabekkajsar89@gmail.com>
2024-10-30 16:29:53 +0500

    ex1

---
+++ exercise/ex1/script.js
@@ -0,0 +1,15 @@
+
+  let  name = 'Qaisar';
+  let  surname = 'Myrzabek';
+  let  byrthday = '24.12.2010';
+  let  school = '№182';
+  let  weight = '45kg';
+  let  height = '160cm';
+   
+
+    console.log("name:" ,name)
+    console.log("familia:" ,surname)
+    console.log("tygan kyni:" ,byrthday)
+    console.log("mektep:" ,school)
+    console.log("salmaq:", weight)
+    console.log("boyi:" ,height)
