bf357b972fc7b46a6bee65d6c72dd131558b379d
Myrzabek Kaisar <myrzabekkajsar89@gmail.com>
2025-01-28 15:39:06 +0500

    ывилырвлаорлоарлыфрлаврыо

---
+++ lesson/28.01.2025/script.js
@@ -0,0 +1,12 @@
+//malimet caqtay
+localStorage.setItem("name", "aijan");
+
+//malimetty oqy
+const name = localStorage.getItem("name");
+console.log(name); //aijan
+
+//malimetti joiy
+localStorage.removeItem("name"); 
+
+//malimetti tazalay
+localStorage.clear(); 
