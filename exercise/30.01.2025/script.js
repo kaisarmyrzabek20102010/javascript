7e9056eca1bc408feef52421c8b72038576e98ee
Myrzabek Kaisar <myrzabekkajsar89@gmail.com>
2025-02-03 15:17:42 +0500

    dfghj

--- exercise/30.01.2025/script.js
+++ exercise/30.01.2025/script.js
@@ -1,4 +1,29 @@
-async function async() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  data = await response.json();
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://67873274c4a42c916105d2fe.mockapi.io/api/onlineduken/films"
+    );
+    if (!response.ok) {
+      throw new Error("qate");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
 }
+
+const clear = document.getElementById("clear");
+const qosy = document.getElementById("qosy");
+
+qosy.addEventListener("click", () => {
+  const div = document.querySelector(".div");
+  div.classList.toggle("visibility");
+});
+
+clear.addEventListener("click", () => {
+  const div = document.querySelector(".div");
+  div.classList.remove("visibility");
+});
+
+fetchData();
